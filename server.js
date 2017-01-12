79280ee3e5b9e7763973539b4d3d7e5a259e3ed0
Xinyang <xinyzhang9@gmail.com>
2017-01-12 02:19:44 -0800

    server

--- server.js
+++ server.js
@@ -20,12 +20,16 @@ var server = app.listen(3000, function(){
 });
 
 var io = require('socket.io')(server);
+
+// observable to monitor source connection
 var sourceConnect = Rx.Observable.create(function(observer){
 	io.on('connection', function(socket){
+		// when socket connected, emit an action
 		socket.emit('mySocketId',{
 			'socketId':socket.id,
 			'connectTime':Date.now()
 		});
+		// when client connected, observer will receive data
 		socket.on('clientConnect',function(data){
 			observer.onNext({
 				'socket':socket,
@@ -39,4 +43,39 @@ var sourceConnect = Rx.Observable.create(function(observer){
 		io.close();
 	}
 
-})
+});
+
+var sourceDisconnect = Rx.Observable.create(function(observable){
+	io.on('connection',function(socket){
+		observable.onNext({
+			'socketId':socket.id,
+			'event': 'clientDisconnect'
+		});
+	});
+	return function(){
+		io.close();
+	}
+});
+
+// subscribe
+var observerConnect = sourceConnect
+	.subscribe(function(obj){
+		var socketId = obj.data.socketId;
+		usersMap = usersMap.set(socketId, obj.data);
+		// emit an action to all users
+		io.emit('allUsers', usersMap.toArray());
+	});
+
+var observerDisconnect = sourceDisconnect
+	.subscribe(function(obj){
+		var socketId = obj.socketId;
+		var user = usersMap.get(socketId);
+		usersMap = usersMap.delete(obj.socketId);
+		// emit an action to all users
+		io.emit('allUsers', usersMap.toArray());
+	})
+
+app.post('/message', function(req,res){
+	// emit an action to display messages
+	io.emit('message',req.body);
+});
