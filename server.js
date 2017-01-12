57f0ff506dcf82029521e9a4c55ceab50a63ca9f
Xinyang <xinyzhang9@gmail.com>
2017-01-12 02:12:51 -0800

    rx

--- server.js
+++ server.js
@@ -1,4 +1,42 @@
+// required modules
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var Rx = require('rx');
+var Immutable = require('immutable');
+var app = express();
+// customized vars
+var usersMap = Immutable.Map({});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use('/',express.static(path.join(__dirname,'client')));
+
+// start server
+var server = app.listen(3000, function(){
+	console.log('listen to port 3000');
+});
+
+var io = require('socket.io')(server);
+var sourceConnect = Rx.Observable.create(function(observer){
+	io.on('connection', function(socket){
+		socket.emit('mySocketId',{
+			'socketId':socket.id,
+			'connectTime':Date.now()
+		});
+		socket.on('clientConnect',function(data){
+			observer.onNext({
+				'socket':socket,
+				'data':data,
+				'event':'clientConnect'
+			});
+		});
+	});
+
+	return function(){
+		io.close();
+	}
+
+})
